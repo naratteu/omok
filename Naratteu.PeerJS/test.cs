379b138f7e5a6ba8180f924d15359f60dd81d4fd
박성원 <skfhddlg@naver.com>
2024-06-23 15:50:20 +0900

    통신 성공

---
+++ Naratteu.PeerJS/test.cs
@@ -0,0 +1,47 @@
+﻿global using Peer = Microsoft.JSInterop.IJSObjectReference;
+global using openPeerHelper = Microsoft.JSInterop.DotNetObjectReference<Naratteu.PeerJS.OnOpen>;
+global using servPeerHelper = Microsoft.JSInterop.DotNetObjectReference<Naratteu.PeerJS.OnServ>;
+using Microsoft.JSInterop;
+
+namespace Naratteu.PeerJS;
+
+partial class test
+{
+    public async ValueTask<(PeerWrap pw, string id)> OpenPeer()
+    {
+        TaskCompletionSource<string> tcs = new();
+        using var oo = DotNetObjectReference.Create(new OnOpen(tcs.SetResult));
+        return (new() { peer = await openPeer(oo), test = this }, await tcs.Task);
+    }
+}
+
+public class OnOpen(Action<string> oo) { [JSInvokable] public void on_open(string id) => oo(id); }
+public class OnServ
+{
+    public required Action<string> oo, od;
+    [JSInvokable] public void on_open(string id) => oo(id);
+    [JSInvokable] public void on_data(string data) => od(data);
+}
+
+public class PeerWrap : IAsyncDisposable
+{
+    ValueTask IAsyncDisposable.DisposeAsync() => peer.DisposeAsync();
+    public required Peer peer { get; init; }
+    public required test test { get; init; }
+
+    public async ValueTask<servPeerHelper> Serv(OnServ on_serv)
+    {
+        var os = DotNetObjectReference.Create(on_serv);
+        await test.servPeer(peer, os);
+        return os;
+    }
+
+    public async ValueTask<(PeerWrap pw, string id)> Conn(string id)
+    {
+        TaskCompletionSource<string> tcs = new();
+        using var oo = DotNetObjectReference.Create(new OnOpen(tcs.SetResult));
+        return (new() { peer = await test.connPeer(peer, id, oo), test = test }, await tcs.Task);
+    }
+
+    public async ValueTask<string> Send(string msg) => await test.send(peer, msg);
+}
--- Naratteu.PeerJS/test.ts
+++ Naratteu.PeerJS/test.ts
@@ -1,4 +1,42 @@
+import { Peer } from "https://esm.sh/peerjs@1.5.4?bundle-deps"
+
 export function echo(msg: string): string
 {
     return msg;
+}
+
+type openPeerHelper = any;
+export function openPeer(dotNetHelper: openPeerHelper): Peer
+{
+    const peer = new Peer();
+    peer.on('open', (id) => { dotNetHelper.invokeMethodAsync('on_open', id); });
+    return peer;
+}
+
+export function connPeer(peer: Peer, id: string, dotNetHelper: openPeerHelper): Peer
+{
+    const conn = peer.connect(id);
+    conn.on('open', () => { dotNetHelper.invokeMethodAsync('on_open', id); });
+    return conn;
+}
+
+type servPeerHelper = any;
+export function servPeer(peer: Peer, dotNetHelper: servPeerHelper): Peer
+{
+    peer.on('connection', (conn) => {
+        conn.on('open', () => { dotNetHelper.invokeMethodAsync('on_open', conn.peer); });
+        conn.on('data', (data) => { dotNetHelper.invokeMethodAsync('on_data', data); });
+    });
+    return peer;
+}
+
+export function send(conn: Peer, msg: string): string
+{
+    conn.send(msg);
+    return msg;
+}
+
+export function createConnectionUrl(id: string): string
+{
+    return window.location.origin + window.location.pathname + "?conn_to=" + id;
 }
